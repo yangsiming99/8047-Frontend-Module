aaf9f75ef483bb89b873ecdefd9d8bf5601bc805
Siming Yang <yangsiming1999@gmail.com>
2024-01-20 17:43:17 -0800

    init commit

---
+++ src/components/Panel.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+    title: String,
+})
+
+</script>
+
+<template>
+    <v-card id="card">
+        <h3>{{ title }}</h3>
+        <slot />
+    </v-card>
+</template>
+
+<style scoped>
+    #card {
+        width: 70vw;
+        height: 60vh;
+        padding: 1.5em;
+    }
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Panel from '../components/Panel.vue';
+
+
+</script>
+
+<template>
+    <Panel title="home">
+        <div>
+            
+        </div>
+    </Panel>
+</template>
+
+<style scoped>
+
+</style>
